668ca9e70fe1a082f0b95f838f6ad6659936ba6c
msadeedashraf <157824309+msadeedashraf@users.noreply.github.com>
2025-02-13 11:13:43 -0500

    .

---
+++ 01_file_system/usingCalulator.js
@@ -0,0 +1,9 @@
+const calculator = require('./mymodule/calculator');
+
+
+console.log(calculator.divide(10,2));
+
+console.log(calculator.add(10,2));
+console.log(calculator.subtract(10,2));
+console.log(calculator.multiply(10,2));
+
