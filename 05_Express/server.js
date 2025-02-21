e3d8e7989c76007169252735e270de998692d15f
msadeedashraf <157824309+msadeedashraf@users.noreply.github.com>
2025-02-21 11:32:45 -0500

    .

--- 05_Express/server.js
+++ 05_Express/server.js
@@ -1,12 +1,28 @@
 //console.log('Hello')
 
+const path = require('path');
+
+
 const express = require('express');
 const app = express();
 //const PORT = 3700;
 
-app.get('/',(req, res)=>{res.send('Hello World')});
+//app.get('/',(req, res)=>{res.send('Hello World')});
 
 //app.listen(PORT, ()=>{console.log('Checking the server '+ PORT)})
 app.listen(3800);
+
+app.get('/', (req, res)=>{
+
+//res.sendFile('./views/index.html', {root:__dirname});
+res.sendFile(path.join(__dirname, 'views', 'index.html'));
+
+});
+
+//app.get('/', (req, res)=>{});
+
+
+
+
 
 
