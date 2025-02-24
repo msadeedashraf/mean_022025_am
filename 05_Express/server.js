fcdf889e665b4effbe4192454411c9f0317d44bd
msadeedashraf <157824309+msadeedashraf@users.noreply.github.com>
2025-02-24 10:00:29 -0500

    .

--- 05_Express/server.js
+++ 05_Express/server.js
@@ -11,14 +11,48 @@ const app = express();
 
 //app.listen(PORT, ()=>{console.log('Checking the server '+ PORT)})
 app.listen(3800);
-
+/*
 app.get('/', (req, res)=>{
 
 //res.sendFile('./views/index.html', {root:__dirname});
 res.sendFile(path.join(__dirname, 'views', 'index.html'));
 
 });
+    
+app.get('/index.html', (req, res)=>{
 
+    res.sendFile(path.join(__dirname, 'views', 'index.html'));
+    
+    });
+*/
+
+
+//Regular Expressions:  https://www.w3schools.com/jsref/jsref_obj_regexp.asp
+
+app.get('^/$|index(.html)?', (req, res)=>{
+
+    res.sendFile(path.join(__dirname, 'views', 'index.html'));
+    
+    });
+
+    app.get('/privacy(.html)?', (req, res)=>{
+
+        res.sendFile(path.join(__dirname, 'views', 'privacy.html'));
+        
+        });
+
+        app.get('/contact(.html)?', (req, res)=>{
+
+            res.sendFile(path.join(__dirname, 'views', 'contact.html'));
+            
+            });
+
+            app.get('/terms(.html)?', (req, res)=>{
+
+                res.sendFile(path.join(__dirname, 'views', 'terms.html'));
+                
+                });
+    
 //app.get('/', (req, res)=>{});
 
 
