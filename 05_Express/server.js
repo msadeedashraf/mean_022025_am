3dbabc559c41ae91eb4a40f7295b030d6eadc085
msadeedashraf <157824309+msadeedashraf@users.noreply.github.com>
2025-02-24 10:32:51 -0500

    .

--- 05_Express/server.js
+++ 05_Express/server.js
@@ -1,9 +1,8 @@
 //console.log('Hello')
 
-const path = require('path');
+const path = require("path");
 
-
-const express = require('express');
+const express = require("express");
 const app = express();
 //const PORT = 3700;
 
@@ -26,37 +25,45 @@ app.get('/index.html', (req, res)=>{
     });
 */
 
-
 //Regular Expressions:  https://www.w3schools.com/jsref/jsref_obj_regexp.asp
 
-app.get('^/$|index(.html)?', (req, res)=>{
+//Routes Section Start
+app.get("^/$|index(.html)?", (req, res) => {
+  res.sendFile(path.join(__dirname, "views", "index.html"));
+});
 
-    res.sendFile(path.join(__dirname, 'views', 'index.html'));
-    
-    });
+app.get("/privacy(.html)?", (req, res) => {
+  res.sendFile(path.join(__dirname, "views", "privacy.html"));
+});
 
-    app.get('/privacy(.html)?', (req, res)=>{
+app.get("/contact(.html)?", (req, res) => {
+  res.sendFile(path.join(__dirname, "views", "contact.html"));
+});
 
-        res.sendFile(path.join(__dirname, 'views', 'privacy.html'));
-        
-        });
+app.get("/terms(.html)?", (req, res) => {
+  res.sendFile(path.join(__dirname, "views", "terms.html"));
+});
 
-        app.get('/contact(.html)?', (req, res)=>{
+app.get("/job_listing(.html)?", (req, res) => {
+  res.sendFile(path.join(__dirname, "views", "job_listing.html"));
+});
 
-            res.sendFile(path.join(__dirname, 'views', 'contact.html'));
-            
-            });
+app.get("/job_search(.html)?", (req, res) => {
+  res.sendFile(path.join(__dirname, "views", "jobs/job_search.html"));
+});
 
-            app.get('/terms(.html)?', (req, res)=>{
 
-                res.sendFile(path.join(__dirname, 'views', 'terms.html'));
-                
-                });
-    
+app.get("/*", (req, res) => {
+    res.sendFile(path.join(__dirname, "views", "error.html"));
+  });
+  
+
+//Routes Section Ends
+
+
+
+//Refactor Routes Section using chaining
+
+//app.get()
+
 //app.get('/', (req, res)=>{});
-
-
-
-
-
-
